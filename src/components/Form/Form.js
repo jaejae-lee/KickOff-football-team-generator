7c90093aa81d52c8db87ac0c6453b99caa3c1d14
jae.L <jklee900414@gmail.com>
2020-01-03 00:53:05 +0000

    position button added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { InputGroup, FormControl, Button }  from 'react-bootstrap';
+import { InputGroup, FormControl, Button, ButtonToolbar, ToggleButtonGroup, ToggleButton }  from 'react-bootstrap';
 
 class Form extends Component {
     constructor(props) {
@@ -64,6 +64,38 @@ class Form extends Component {
                     value={ player }
                     onChange={ this.handleChangeName }  
                     />
+                    {/* <DropdownButton
+                    as={InputGroup.Append}
+                    variant="outline-secondary"
+                    title="Dropdown"
+                    id="input-group-dropdown-2"
+                    >
+                    <Dropdown.Item>Position1</Dropdown.Item>
+                    <Dropdown.Item>Position2</Dropdown.Item>
+                    <Dropdown.Item>Position3</Dropdown.Item>
+                    <Dropdown.Item>Position4</Dropdown.Item>
+                    <Dropdown.Item>Position5</Dropdown.Item>
+                    </DropdownButton> */}
+
+                    {/* <ButtonToolbar aria-label="Toolbar with button groups">
+                    <ButtonGroup className="positionBtnGroup mr-2" aria-label="First group">
+                        <Button className="button">Defender</Button>
+                        <Button className="button">GoalKeeper</Button>
+                        <Button className="button">Striker</Button>
+                        <Button className="button">MidFielder</Button>
+                        <Button className="button">else</Button>
+                    </ButtonGroup>
+                    </ButtonToolbar> */}
+
+                    <ButtonToolbar>
+                        <ToggleButtonGroup className="buttonContainer" type="radio" name="options" defaultValue={1}>
+                            <ToggleButton className="button positionBtn" value={1}>Anything</ToggleButton>
+                            <ToggleButton className="button positionBtn" value={2}>Defender</ToggleButton>
+                            <ToggleButton className="button positionBtn" value={3}>Goalkeeper</ToggleButton>
+                            <ToggleButton className="button positionBtn" value={4}>Striker</ToggleButton>
+                            <ToggleButton className="button positionBtn" value={5}>Midfielder</ToggleButton>
+                        </ToggleButtonGroup>
+                    </ButtonToolbar>
 
                     { !nameError? null : 
                         <p className= "errorMessage"> Please enter player's name
