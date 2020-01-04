39b90ce93c0fe51c8be5c7951adb06acbec177ae
jae.L <jklee900414@gmail.com>
2020-01-04 13:12:29 +0000

    generated two teams based-on teamA size(user input) fixed

--- src/actions/state.js
+++ src/actions/state.js
@@ -18,12 +18,6 @@ export const reset = () => {
 export const generateTeams = (playerList) => {
 
     function shuffleArray (array) {
-        /*
-        console.log(array.length)
-        array.map((item, index)=>{
-            const j = Math.floor(Math.random() * array.length);
-            array[array.length-1] = array.splice(j,1)[0]
-        });*/
 
         for (let i = array.length - 1; i === 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -37,26 +31,18 @@ export const generateTeams = (playerList) => {
     const teamA = []; 
     const teamB = []; 
 
-    
-    //이걸로 teamA 랑 teamB 두개 배열로 나눴는데, teamB에만 자꾸 맨 마지막에? undefinded가 하나씩 들어가요 
-    
-     for(let i = 0; i <= shuffledplayerList.length-1; i++){
-         
-            if(teamA.length <= shuffledplayerList.teamAsize){
-                teamA.push(shuffledplayerList[i]);
-            }else{
-                teamB.push(shuffledplayerList[i]);
-            }
-    } 
-    
-    /*
+    console.log(playerList);
+    console.log(playerList.teamAsize);
+    console.log(shuffledplayerList);
+
+    //generated two arrays
     shuffledplayerList.map((item , index) => {
-        if(teamA.length <= shuffledplayerList.teamAsize){
+        if(teamA.length <= playerList.teamAsize){
             teamA.push(shuffledplayerList[index]);
         }else{
             teamB.push(shuffledplayerList[index]);
         }
-    })*/
+    })
 
     return {
         type: "generateTeams",
