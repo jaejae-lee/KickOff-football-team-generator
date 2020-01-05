821689e0f491a2540c99bc34e6a3094712f44d58
jae.L <jklee900414@gmail.com>
2020-01-05 18:43:33 +0000

    Updates

---
+++ precache-manifest.85c7c504da596e8596d61d84055aed07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb7677369a6057f7d18da632f9795c36",
+    "url": "/KickOff-football-team-generator/index.html"
+  },
+  {
+    "revision": "5491601943b407f88b43",
+    "url": "/KickOff-football-team-generator/static/css/main.b8c2cf9a.chunk.css"
+  },
+  {
+    "revision": "38ca27c61d50ed46ef29",
+    "url": "/KickOff-football-team-generator/static/js/2.3f5cd8d0.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/KickOff-football-team-generator/static/js/2.3f5cd8d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5491601943b407f88b43",
+    "url": "/KickOff-football-team-generator/static/js/main.3a6e8800.chunk.js"
+  },
+  {
+    "revision": "dc59d8e0cef17ede3fa6",
+    "url": "/KickOff-football-team-generator/static/js/runtime-main.dd40c589.js"
+  },
+  {
+    "revision": "466df5512961d216e74346fd76a28d19",
+    "url": "/KickOff-football-team-generator/static/media/bigScreen.466df551.jpg"
+  },
+  {
+    "revision": "afb41ea83c62f77ae1f5eabfff5bcac6",
+    "url": "/KickOff-football-team-generator/static/media/smallScreen.afb41ea8.jpg"
+  }
+]);
